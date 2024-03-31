1e4d33d2be64332a5a860b77e196843dfc276ee7
gdp1988 <378416993@qq.com>
2024-03-31 11:45:37 +0800

    Update constants.ts

--- next/src/utils/constants.ts
+++ next/src/utils/constants.ts
@@ -5,16 +5,17 @@ export const GPT_35_TURBO = "gpt-3.5-turbo" as const;
 export const GPT_4 = "gpt-4" as const;
 export const GPT_MODEL_NAMES = [GPT_35_TURBO, GPT_4];
 
-export const DEFAULT_MAX_LOOPS_FREE = 25 as const;
-export const DEFAULT_MAX_LOOPS_CUSTOM_API_KEY = 10 as const;
+export const DEFAULT_MAX_LOOPS_FREE = 100 as const;
+export const DEFAULT_MAX_LOOPS_CUSTOM_API_KEY = 100 as const;
 
+export const DEFAULT_API_URL = 'https://api.openai.com/v1'
 export const getDefaultModelSettings = (): ModelSettings => {
   return {
     customApiKey: "",
-    language: ENGLISH,
-    customModelName: GPT_35_TURBO,
+    language: CHINESE,
+    customModelName: GPT_4,
     customTemperature: 0.8,
     customMaxLoops: DEFAULT_MAX_LOOPS_FREE,
-    maxTokens: 1250,
+    maxTokens: 7250,
   };
 };
